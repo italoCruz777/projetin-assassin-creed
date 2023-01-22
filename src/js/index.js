fbd5e3df2f037880b0a253721155b8009ed0dde4
italoCruz777 <123280343+italoCruz777@users.noreply.github.com>
2023-01-21 22:54:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+console.log('mostrar o document', document);
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
